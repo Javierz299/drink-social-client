3837d63df01dd947970d1486a088bc856f8652fb
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2021-02-03 20:20:05 -0500

    custom messages added to beer carousel. next add amount drink is equal too

--- src/components/AlcoholForm/AlcoholForm.js
+++ src/components/AlcoholForm/AlcoholForm.js
@@ -4,18 +4,27 @@ import liquorStore from '../../LiquorStore/LiquorStore';
 import { SUBMIT_DRINK } from '../../store/actions/action_types';
 
 import './alcoholForm.css'
-const AlcoholForm = (item) => {
-    const userDrinkItem = useSelector(userDrinkItem => userDrinkItem.user_reducer.drink_item);
+const AlcoholForm = () => {
+    const userDrinkItem = useSelector(userDrinkItem => userDrinkItem.user_reducer.drink_item[0]);
+    const userDrinkMessage = useSelector(userDrinkMessage => userDrinkMessage.user_reducer.drink_item[1]);
     const dispatch = useDispatch();
+
     const handlePostDrink = () => {
-        console.log("beer me clicked")
+        console.log("beer me clicked",userDrinkItem)
+        dispatch({type: SUBMIT_DRINK, payload: false})
+        //post route
+    }
+
+    const handleDrink = () => {
         dispatch({type: SUBMIT_DRINK, payload: false})
     }
+
     return (
         <div className="alcoholForm">
-            <h1>Drinking a {userDrinkItem}, noice!</h1>
-            <button>nope</button>
-            <button onClick={() => handlePostDrink()}>beer me</button>
+            <h6>Don't drink and drive!</h6>
+            <h4>{userDrinkMessage}</h4>
+            <button onClick={() => handleDrink()}>cancel</button>
+            <button onClick={() => handlePostDrink()}>Add Drink</button>
         </div>
     )
 }
