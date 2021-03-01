073f7b3a53b45bc6f8b7b8f83a333e72368cd765
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2021-03-01 15:47:40 -0500

    changed endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
  const config = {
-    API_ENDPOINT: process.env.API_ENDPOINT,
+    API_ENDPOINT: "https://guarded-reef-28707.herokuapp.com/api",
     SKIP_PREFLIGHT_CHECK: process.env.REACT_APP_SKIP_PREFLIGHT_CHECK
 }
 
