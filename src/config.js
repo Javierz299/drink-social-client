2d78a9d618fbbb91dcd36ea735a36ff7b1cfb8cc
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2021-03-01 15:42:48 -0500

    settin up for vercel

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
  const config = {
-    API_ENDPOINT: "http://localhost:8000/api",
+    API_ENDPOINT: process.env.API_ENDPOINT,
+    SKIP_PREFLIGHT_CHECK: process.env.REACT_APP_SKIP_PREFLIGHT_CHECK
 }
+
 
 export default config;
