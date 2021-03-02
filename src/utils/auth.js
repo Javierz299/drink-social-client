6248d67937230a37016439ab12c391ea282fe0bf
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2021-03-01 19:53:32 -0500

    added new url to redirectUri in auth.js and added them to authO

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,7 +5,7 @@ class Auth {
     auth0 = new auth0.WebAuth({
         domain: 'dev-u9j3svni.auth0.com',
         clientID: 'whwkSL7cwzB4fskmSWSwwo43aVIFupY4',
-        redirectUri: "http://localhost:3000/callback",
+        redirectUri: "https://drink-social-zapien120.vercel.app/callback" || "http://localhost:3000/callback",
         responseType: 'token id_token',
         scope: 'openid profile email'
     });
