2ab34d9a256dd0b798e2a32146c8561d1076c803
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2021-03-18 01:38:44 -0400

    added graph. seems to be working

--- src/components/PublicGraph/PublicGraph.js
+++ src/components/PublicGraph/PublicGraph.js
@@ -1,18 +1,63 @@
-import React, { useEffect }  from 'react';
+import React, { useState, useEffect }  from 'react';
+import {Bar} from 'react-chartjs-2';
 
 import axios from 'axios';
 import config from '../../config';
 
 const PublicGraph = () => {
-
+    const [drink, setDrink] = useState(0)
+    const state = {
+        labels: ['Beer', 'Cocktail', 'Wine',
+                 'Liquor', 'Other'],
+        datasets: [
+          {
+            label: '',
+            backgroundColor: [
+              '#B21F00',
+              '#C9DE00',
+              '#2FDE00',
+              '#00A6B4',
+              '#6800B4'
+            ],
+            hoverBackgroundColor: [
+            '#501800',
+            '#4B5000',
+            '#175000',
+            '#003350',
+            '#35014F'
+            ],
+            data: [
+                drink.beerTotal,
+                drink.cocktailTotal,
+                drink.wineTotal,
+                drink.liquorTotal,
+                drink.bingeTotal,
+                ]
+          }
+        ]
+      }
     useEffect(() => {
         axios.get(`${config.API_ENDPOINT}/get/cumulativeDrinks`)
-            .then((result) => console.log("all drinks",result.data))
+            .then((result) => setDrink(result.data))
        
     }, [])
+    console.log("drinks",drink)
     return (
         <div>
-            
+            <Bar
+          data={state}
+          options={{
+            title:{
+              display:true,
+              text:'Cumulative Drinks',
+              fontSize:20,
+            },
+            legend:{
+              display:false,
+              position:'right'
+            }
+          }}
+        />
         </div>
     )
 }
