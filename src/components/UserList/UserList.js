a7ab347e9c63a2899fddda3d38d24ebc712bf4cd
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2021-04-12 22:59:41 -0400

    added button for users. consolelogging corresponding id

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -7,6 +7,9 @@ const UserList = ({users}) => {
             {users && users.map(user => (
                 <li key={user.id}>
                     {user.username}
+                    <button onClick={() =>console.log("added friend",user.id)}>
+                        add
+                    </button>
                 </li>
             ))}
         </div>
