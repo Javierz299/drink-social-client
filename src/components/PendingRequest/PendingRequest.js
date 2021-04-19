bbf7673be27cb4bc7251dcecb7a295ecc4c82508
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2021-04-19 00:45:29 -0400

    adding friend and updating db with true is working. need to filter out pending after adding user. and filter request if user picks decline with updating the db by deleting o.g. request

--- src/components/PendingRequest/PendingRequest.js
+++ src/components/PendingRequest/PendingRequest.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import config from '../../config';
+import axios from 'axios';
+
 import { useSelector } from 'react-redux';
 
 import { Link } from 'react-router-dom';
@@ -7,15 +10,21 @@ import { Link } from 'react-router-dom';
 const PendingRequest = () => {
     const pendingRequests = useSelector(pending => pending.pending_reducer.pending_requests)
 
+    const addFriendRequest = (addUser) => {
+        console.log("adduser",addUser)
+        axios.patch(`${config.API_ENDPOINT}/add/friend`,addUser)
+            .then(res => console.log("patch result",res))
+    }
+
     return (
         <div>
             <Link to="/friends">friends</Link>
             <Link to="/profile">Profile</Link>
             <div>
-                {pendingRequests.length < 1 && <h3>no pending requests</h3>}
-               {pendingRequests.map((pending,i) => (
+                {!pendingRequests.length && <h3>no pending requests</h3>}
+                {pendingRequests.map((pending,i) => (
                    //if user declines delete request from db
-                   <li key={i} >{pending.user} <button onClick={() => console.log("add", pending.user)}>add</button><button>decline</button></li>
+                   <li key={i} >{pending.user} <button onClick={() => addFriendRequest(pending)}>add</button><button>decline</button></li>
                ))}
             </div>
         </div>
